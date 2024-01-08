9aa81a4fbd16a096d36d422421b694ac24fc12b0
Alexander Bartley Nees <alexbartleynees@gmail.com>
2024-01-08 17:14:21 +1300

    Update workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+  prefetch: true,
   site: "https://alex-bartleynees.github.io/",
   base: "/portfolio_v2",
 });
