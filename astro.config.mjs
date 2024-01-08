4ee5bd8d95c9006e352c787552bf7fb48ec0c635
Alexander Bartley Nees <alexbartleynees@gmail.com>
2024-01-08 17:08:40 +1300

    Add deployment steps

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,6 @@ export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   prefetch: true,
+  site: "https://alex-bartleynees.github.io/",
+  base: "/portfolio_v2",
 });
