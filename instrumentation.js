9ed86fa363a23e0e0bd2a677c9b778568dae1c85
Alex Bartley Nees <alexbartleynees@gmail.com>
2025-02-07 09:54:11 +1300

    Use esmodule syntax

--- instrumentation.js
+++ instrumentation.js
@@ -1,26 +1,14 @@
-const { NodeSDK } = require("@opentelemetry/sdk-node");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
-const {
-  getNodeAutoInstrumentations,
-} = require("@opentelemetry/auto-instrumentations-node");
-const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} = require("@opentelemetry/sdk-metrics");
-const {
-  OTLPTraceExporter,
-} = require("@opentelemetry/exporter-trace-otlp-proto");
-const {
-  OTLPMetricExporter,
-} = require("@opentelemetry/exporter-metrics-otlp-proto");
-const { Resource } = require("@opentelemetry/resources");
-const {
-  SemanticResourceAttributes,
-} = require("@opentelemetry/semantic-conventions");
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+import { Resource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 
 const sdk = new NodeSDK({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "portfolio-site",
+    [SemanticResourceAttributes.SERVICE_NAME]: "your-service-name",
   }),
   traceExporter: new OTLPTraceExporter({
     url: "http://my-collector-collector.monitoring.svc.cluster.local:4318/v1/traces",
